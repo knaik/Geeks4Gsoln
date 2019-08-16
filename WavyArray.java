ba16c1426c423ee07fe4694f48ffc750adc09bdd
Karan Naik <12472923+knaik94@users.noreply.github.com>
2019-08-15 23:44:19 -0400

    Update WavyArray.java

--- WavyArray.java
+++ WavyArray.java
@@ -66,3 +66,104 @@ class GFG
 	 }
 	 }
 }
+
+
+//better optimized?
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+ {
+	public static void main (String[] args)
+	 {
+	 //code
+	 Scanner scan = new Scanner(System.in);
+	 int Cases = scan.nextInt();
+	 ArrayList<int[]> arrays = new ArrayList<int[]>();
+	 for (int i = 0; i < Cases; i++){
+	     int len = scan.nextInt();
+	     arrays.add(new int[len]);
+	     for (int j = 0; j < len; j++){
+	         arrays.get(i)[j] = scan.nextInt();
+	     }
+	     //System.out.println(Arrays.toString(arrays.get(i)));
+	 }
+	 
+	 ArrayList<int[]> waves = new ArrayList<int[]>();
+	 for (int k = 0; k < Cases; k++){
+	     int[] temp = arrays.get(k);
+	     for (int p = 0; p < temp.length-1; p+=2){
+	         int hold = temp[p+1];
+	         temp[p+1]=temp[p];
+	         temp[p] = hold;
+	     }
+	     
+	     
+	     System.out.println(Arrays.toString(temp));
+	     
+	 }
+	     
+	     String prn = "";
+         
+         for (int w = 0 ; w < temp.length; w++){
+             if (temp[w] == 0 && w > 1){
+                 prn += "";
+             }
+             else{
+                 prn += temp[w]+" ";
+             }
+            
+         }
+         System.out.println(prn.trim());
+
+	 }
+	     
+	 }
+}
+
+
+
+
+
+
+//final accepted solution
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+ {
+	public static void main (String[] args)
+	 {
+	 //code
+	 Scanner scan = new Scanner(System.in);
+	 int Cases = scan.nextInt();
+	 ArrayList<int[]> arrays = new ArrayList<int[]>();
+	 for (int i = 0; i < Cases; i++){
+	     int len = scan.nextInt();
+	     arrays.add(new int[len]);
+	     for (int j = 0; j < len; j++){
+	         arrays.get(i)[j] = scan.nextInt();
+	     }
+	     //System.out.println(Arrays.toString(arrays.get(i)));
+	 }
+	 
+	 ArrayList<int[]> waves = new ArrayList<int[]>();
+	 for (int k = 0; k < Cases; k++){
+	     int[] temp = arrays.get(k);
+	     for (int p = 0; p < temp.length-1; p+=2){
+	         int hold = temp[p+1];
+	         temp[p+1]=temp[p];
+	         temp[p] = hold;
+	     }
+	     
+	     
+	     System.out.println(Arrays.toString(temp).replace("[", "").replace("]", "").replace(",",""));
+	     
+	 }
+	     
+	     
+	     
+	 }
+}
